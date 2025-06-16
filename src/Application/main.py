3c48a06c9d1625b7ffbc67b9d1a09e2d03ba86ba
G3nKai <kulikovila2005@gmail.com>
2025-06-16 10:01:48 +0700

    тестовый инит

--- src/Application/main.py
+++ src/Application/main.py
@@ -2,6 +2,21 @@ from typing import Union
 from pydantic import BaseModel
 from fastapi import FastAPI
 
+from Infrastructure.database import engine, sessionLocal
+from Domain.Models.person import Person
+
+def test_db():
+    db = sessionLocal()
+    try:
+        persons = db.query(Person).all()
+        print(persons)
+    finally:
+        db.close()
+
+if __name__ == "__main__":
+    test_db()
+
+
 app = FastAPI()
 
 class Item(BaseModel):
@@ -12,7 +27,6 @@ class Item(BaseModel):
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
